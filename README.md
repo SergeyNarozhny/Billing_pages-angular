f29a219748274906428715a8a49a82cce97ede94
Sergey Narozhnyy <sergey.narozhnyy@gmail.com>
2018-01-18 10:10:35 +0000

    Updates for c9z.ru

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Simple billing pages app in Angular v1.5
 
-## Dependencies are listed in package.json. Simple install through npm install
-
-Working example is here: http://ec2-35-162-151-181.us-west-2.compute.amazonaws.com:8820/
+Working example is here: http://c9z.ru:8820/
--- bill.config.js
+++ bill.config.js
@@ -2,15 +2,16 @@
  * Routes configuration
  * @ngInject [$routeProvider]
  */
-function routing($routeProvider) {
+function routing($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: './static/view/deposit.html',
+			templateUrl: '/static/view/deposit.html',
 		})
 		.when('/pay/', {
-			templateUrl: './static/view/payment.html',
+			templateUrl: '/static/view/payment.html',
 		});
+	$locationProvider.hashPrefix('');
 }
-routing.$inject = ['$routeProvider'];
+routing.$inject = ['$routeProvider', '$locationProvider'];
 
 export default routing;
